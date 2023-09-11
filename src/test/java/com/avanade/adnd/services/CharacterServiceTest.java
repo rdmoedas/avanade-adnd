4c44c267aaf59f6fdc29b0f06fd319d090366e7f
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-11 02:34:46 -0300

    test(character service): implement character service tests

--- src/test/java/com/avanade/adnd/services/CharacterServiceTest.java
+++ src/test/java/com/avanade/adnd/services/CharacterServiceTest.java
@@ -3,6 +3,7 @@ package com.avanade.adnd.services;
 import com.avanade.adnd.model.Character;
 import com.avanade.adnd.payloads.CreateCharacterRequest;
 import com.avanade.adnd.repository.CharacterRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -24,6 +26,11 @@ class CharacterServiceTest {
     @Mock
     private CharacterRepository characterRepository;
 
+//    @Before("")
+//    public void setup() {
+//        characterService = new CharacterService(characterRepository);
+//    }
+    
     @Test
     @DisplayName("Should insert character with no errors")
     void shouldInsertCharacterWithNoErrors() {
@@ -38,7 +45,7 @@ class CharacterServiceTest {
 
     @Test
     @DisplayName("Should find character by id with no errors")
-    void shouldFindCharacterByIdWithNoErrors() {
+    void shouldFindCharacterByIdWithNoErrors() throws Exception {
         // GIVEN
         Character character = mock(Character.class);
         when(characterRepository.findById(anyLong()))
@@ -60,7 +67,8 @@ class CharacterServiceTest {
         when(characterRepository.findAll())
                 .thenReturn(List.of(character));
         // WHEN
-        List<Character> result = characterService.findAllCharacters();
+        Pageable pageable = mock(Pageable.class);
+        List<Character> result = characterService.findAllCharacters(pageable);
         // THEN
         Assertions.assertNotNull(result);
         Assertions.assertEquals(List.of(character), result);
@@ -70,29 +78,40 @@ class CharacterServiceTest {
 
     @Test
     @DisplayName("Should update character with no errors")
-    void shouldUpdateCharacterWithNoErrors() {
+    void shouldUpdateCharacterWithNoErrors() throws Exception { // TODO CONTINUAR AQUI
         // GIVEN
-        Character character = mock(Character.class);
+        Character character = new Character();
+        Long id = (long) 1L;
+        character.setId(id);
+        character.setName("Test");
         when(characterRepository.findById(anyLong()))
                 .thenReturn(java.util.Optional.of(character));
         // WHEN
-        Assertions.assertDoesNotThrow(() -> characterService.updateCharacter(character));
+        CreateCharacterRequest createCharacterRequest = mock(CreateCharacterRequest.class);
+        when(createCharacterRequest.name())
+                .thenReturn("Unit Test");
+        Character result = characterService.updateCharacter(id.toString(), createCharacterRequest);
         // THEN
+        Assertions.assertDoesNotThrow(() -> characterService.updateCharacter(id.toString(), createCharacterRequest));
+//        Assertions.assertNotNull(result);
+        Assertions.assertEquals("Unit Test", result.getName());
         verify(characterRepository, times(1))
                 .save(any(Character.class));
+
     }
 
     @Test
     @DisplayName("Should delete character with no errors")
     void shouldDeleteCharacterWithNoErrors() {
         // GIVEN
-        Character character = mock(Character.class);
+        Character character = new Character();
+        character.setId((long) 1L);
         when(characterRepository.findById(anyLong()))
                 .thenReturn(java.util.Optional.of(character));
         // WHEN
-        Assertions.assertDoesNotThrow(() -> characterService.deleteCharacterById(1L));
+        Assertions.assertDoesNotThrow(() -> characterService.deleteCharacterById((long) 1L));
         // THEN
         verify(characterRepository, times(1))
-                .delete(any(Character.class));
+                .deleteById((long) 1L);
     }
 }
