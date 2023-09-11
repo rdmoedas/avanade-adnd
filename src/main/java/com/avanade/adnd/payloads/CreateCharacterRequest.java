7b024988b0fba7035b6e946549f378cbadd94a24
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-11 00:00:09 -0300

    feat(character payload): add CreateCharacterRequest

---
+++ src/main/java/com/avanade/adnd/payloads/CreateCharacterRequest.java
@@ -0,0 +1,16 @@
+package com.avanade.adnd.payloads;
+
+import com.avanade.adnd.model.CharacterType;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateCharacterRequest(
+        @NotNull CharacterType type,
+        @NotNull String name,
+        @NotNull int hitPoints,
+        @NotNull int strength,
+        @NotNull int defense,
+        @NotNull int agility,
+        @NotNull int diceAmount,
+        @NotNull int diceSides
+        ) {
+}
