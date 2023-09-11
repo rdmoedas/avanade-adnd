dd27d22a8a0113289e2ae2b8535cf457e41ee71b
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-11 02:39:18 -0300

    test(health-check): add display name and throws exception

--- src/test/java/com/avanade/adnd/HealthCheckTest.java
+++ src/test/java/com/avanade/adnd/HealthCheckTest.java
@@ -1,5 +1,6 @@
 package com.avanade.adnd;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
