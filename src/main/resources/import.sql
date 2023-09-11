01e249a76dfa246accec3b8e03b51527adab1272
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-11 02:33:30 -0300

    feat(import sql): remove id from dev example sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO character ("type", "name", "hit_points", "strength",  "defense", "agility", "dice_amount", "dice_sides") VALUES ('HERO', 'Warrior', 20, 7, 5, 6, 1, 12);
+INSERT INTO character ("type", "name", "hit_points", "strength",  "defense", "agility", "dice_amount", "dice_sides") VALUES ('HERO', 'Barbarian', 21, 10, 2, 5, 2, 8);
+INSERT INTO character ("type", "name", "hit_points", "strength",  "defense", "agility", "dice_amount", "dice_sides") VALUES ('HERO', 'Knight', 26, 6, 8, 3, 2, 6);
+INSERT INTO character ("type", "name", "hit_points", "strength",  "defense", "agility", "dice_amount", "dice_sides") VALUES ('MONSTER', 'Orc', 42, 7, 1, 2, 3, 4);
+INSERT INTO character ("type", "name", "hit_points", "strength",  "defense", "agility", "dice_amount", "dice_sides") VALUES ('MONSTER', 'Giant', 34, 10, 4, 4, 2, 6);
+INSERT INTO character ("type", "name", "hit_points", "strength",  "defense", "agility", "dice_amount", "dice_sides") VALUES ('MONSTER', 'Werewolf', 34, 7, 4, 7, 2, 4);
