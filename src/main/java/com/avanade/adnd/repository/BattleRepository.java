dbd791d59ab627fe825bdab16677be0404d21d95
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-13 08:25:35 -0300

    feat(battle repository): add findAllByPlayerCharacterId

--- src/main/java/com/avanade/adnd/repository/BattleRepository.java
+++ src/main/java/com/avanade/adnd/repository/BattleRepository.java
@@ -4,8 +4,10 @@ import com.avanade.adnd.model.Battle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface BattleRepository extends JpaRepository<Battle, UUID> {
+public interface BattleRepository extends JpaRepository<Battle, Long> {
+    List<Battle> findAllByPlayerCharacterId(UUID id);
 }
