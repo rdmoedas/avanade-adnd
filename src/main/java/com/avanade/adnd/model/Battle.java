687877638c3f6dc131cb14918ebeb13b95821237
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-13 08:18:43 -0300

    feat(battle): update battle entity

--- src/main/java/com/avanade/adnd/model/Battle.java
+++ src/main/java/com/avanade/adnd/model/Battle.java
@@ -1,37 +1,76 @@
 package com.avanade.adnd.model;
 
 import com.avanade.adnd.model.enums.BattleStatus;
+import com.avanade.adnd.model.enums.Initiative;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "battle")
 public class Battle {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne
-    @Column(nullable = false)
     private PlayerCharacter playerCharacter;
 
     @ManyToOne
-    @Column(nullable = false)
-    private Character enemyCharacter;
+    private Character enemy;
 
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER)
     private List<BattleLog> battleLogs;
 
     @Enumerated(EnumType.STRING)
     private BattleStatus status;
 
-    private Boolean playerHasInitiative;
+    @Enumerated(EnumType.STRING)
+    private Initiative initiative;
 
-    private Boolean playerCharacterWon;
+    public int getPlayerHitPoints() {
+        return this.playerCharacter.getCharacter().getHitPoints();
+    }
+
+    public int getEnemyHitPoints() {
+        return this.enemy.getHitPoints();
+    }
+
+    public int getPlayerStrength() {
+        return this.playerCharacter.getCharacter().getStrength();
+    }
+
+    public int getEnemyStrength() {
+        return this.enemy.getStrength();
+    }
+
+    public int getPlayerAgility() {
+        return this.playerCharacter.getCharacter().getAgility();
+    }
+
+    public int getEnemyAgility() {
+        return this.enemy.getAgility();
+    }
+
+    public int getPlayerDefense() {
+        return this.playerCharacter.getCharacter().getDefense();
+    }
+
+    public int getEnemyDefense() {
+        return this.enemy.getDefense();
+    }
+
+    public int getPlayerDiceAmount() {
+        return this.playerCharacter.getCharacter().getDiceAmount();
+    }
+
+    public int getPlayerDiceSides() {
+        return this.playerCharacter.getCharacter().getDiceSides();
+    }
 
 }
