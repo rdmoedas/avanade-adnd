e4cbfe487922714e8a293d8191ec008e99766a7c
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-14 08:11:47 -0300

    feat(battle service): choice random enemy if the player don't choice one

--- src/main/java/com/avanade/adnd/repository/CharacterRepository.java
+++ src/main/java/com/avanade/adnd/repository/CharacterRepository.java
@@ -1,9 +1,13 @@
 package com.avanade.adnd.repository;
 
 import com.avanade.adnd.model.Character;
+import com.avanade.adnd.model.enums.CharacterType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+    List<Character> findAllByType(CharacterType type);
 }
