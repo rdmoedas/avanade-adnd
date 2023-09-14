b03aab92fc28b072be2b3cb3be11aa2c44611b64
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-14 08:05:04 -0300

    fix(battleLog entity): remove unique from turn

--- src/main/java/com/avanade/adnd/model/BattleLog.java
+++ src/main/java/com/avanade/adnd/model/BattleLog.java
@@ -20,7 +20,7 @@ public class BattleLog {
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonProperty()
     private Battle battle;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private int turn;
     @Enumerated(EnumType.STRING)
     private TurnStatus status;
