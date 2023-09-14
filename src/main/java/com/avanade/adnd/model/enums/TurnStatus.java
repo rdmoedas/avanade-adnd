aedd78d603bbcb1c6cd723b92b4bf3965b33aaa5
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-14 04:39:46 -0300

    feat(battle-log enum): add description in turnStatus

--- src/main/java/com/avanade/adnd/model/enums/TurnStatus.java
+++ src/main/java/com/avanade/adnd/model/enums/TurnStatus.java
@@ -1,8 +1,18 @@
 package com.avanade.adnd.model.enums;
 
 public enum TurnStatus {
-    FINISHED,
-    AWAITING_PLAYER_DEFENSE,
-    AWAITING_PLAYER_ATTACK,
-    AWAITING_PLAYER_DAMAGE,
+    FINISHED("Turn finished"),
+    AWAITING_PLAYER_DEFENSE("Defend!"),
+    AWAITING_PLAYER_ATTACK("Attack!"),
+    AWAITING_PLAYER_DAMAGE("Roll damage!");
+
+    private final String description;
+
+    TurnStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
 }
